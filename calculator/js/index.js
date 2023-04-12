d6b29c84125bbe519dcad2d6ae2e9f4ca10bc60a
Prem Raj <premraj299341@gmail.com>
2023-04-12 16:50:02 +0530

    add js to calculator and remove function not working

---
+++ calculator/js/index.js
@@ -0,0 +1,21 @@
+let result = document.getElementById("result");
+
+let value;
+
+function addToResult(value) {
+    result.value += value;
+}
+
+function calculate() {
+    result.value = eval(result.value);
+}
+
+function clearResult() {
+    result.value = '';
+}
+
+// remove buttom not work properly
+function remove () {
+    result.value = result.value.substring(0, result.length-1);
+    console.log(result.value);
+}
