c61044630dd97b45546506e8f553653565235da8
Prem Raj <premraj299341@gmail.com>
2023-05-05 22:23:18 +0530

    change stylesheet to calculator and js change

--- calculator/js/index.js
+++ calculator/js/index.js
@@ -16,6 +16,9 @@ function clearResult() {
 
 // remove buttom not work properly
 function remove () {
-    result.value = result.value.slice(0, result.length-1);
-    console.log(result.value);
+    let substring = result.value;
+    substring = substring.slice(0 , substring.length -1);
+    result.value = substring;
+    console.log(substring.length);
+    console.log(substring);
 }
